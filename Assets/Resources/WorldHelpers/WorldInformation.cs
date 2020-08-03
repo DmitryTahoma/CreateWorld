493d0c661f5885661d3ef62f7ae1ee2d9c30cfd5
DmitryTahoma <dimon.otrishk@gmail.com>
2020-08-03 10:43:40 +0300

    added plane generation

---
+++ Assets/Resources/WorldHelpers/WorldInformation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public static class WorldInformation
+{
+    public static byte[,] Blocks { private set; get; }
+    public static int Width { set; get; } = 20;
+    public static int Depth { private set; get; } = 5;
+
+    static WorldInformation()
+    {
+        Blocks = new byte[Width, Depth];
+        for (int i = 0; i < Width; ++i)
+            for (int j = 0; j < Depth; ++j)
+            {
+                if (j == Depth - 1)
+                    Blocks[i, j] = 0;
+                else
+                    Blocks[i, j] = 1;
+            }
+    }
+
+    public static GameObject GetObjectById(byte id)
+    {
+        string name;
+        switch(id)
+        {
+            default: name = "grass"; break;
+            case 1: name = "ground"; break;
+        };
+        return Resources.Load("Blocks/" + name) as GameObject;
+    }
+
+    public static GameObject GetObjectById(int x, int y)
+        => GetObjectById(Blocks[x, y]);
+}
---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+    void Start()
+    {
+        for(int i = 0; i < WorldInformation.Width; ++i)
+            for(int j = 0; j < WorldInformation.Depth; ++j)
+            {
+                if (WorldInformation.Blocks[i, j] != 255) // object with id = 255 is air
+                {
+                    GameObject block = Instantiate(WorldInformation.GetObjectById(i, j));
+                    block.transform.position = new Vector2(i - WorldInformation.Width / 2 + 0.5f, j);
+                }
+            }
+    }
+}
