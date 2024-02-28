26af47ee232d1262fd4118401668def694f7cf6b
DmitryTahoma <dimon.otrishk@gmail.com>
2024-02-28 16:28:47 +0200

    Added check whether the cursor is over the interface

--- Assets/Scripts/Input/GameInput.cs
+++ Assets/Scripts/Input/GameInput.cs
@@ -10,6 +10,7 @@ public class GameInput : MonoBehaviour
 	private InputAction moveInputAction;
 	private InputAction scrollInputAction;
 
+	[SerializeField] private GameObject canvas;
 	[SerializeField] private MouseClickableBase[] leftMouseHandlers;
 
 	private void Awake()
@@ -50,9 +51,27 @@ public class GameInput : MonoBehaviour
 		return Camera.main.rect.Contains(GetMouseViewportPosition());
 	}
 
+	public bool IsCursorOverUI()
+	{
+		for (int i = 0; i < canvas.transform.childCount; ++i)
+		{
+			Transform child = canvas.transform.GetChild(i);
+			RectTransform rectTransform = child.GetComponent<RectTransform>();
+			
+			if (child.gameObject.activeSelf
+				&& rectTransform != null
+				&& RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, GetMousePosition(), null, out Vector2 localPoint)
+				&& rectTransform.rect.Contains(localPoint))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public void LeftClickHandle(InputAction.CallbackContext context)
 	{
-		if (!IsCursorOverWindow())
+		if (!IsCursorOverWindow() || IsCursorOverUI())
 		{
 			return;
 		}
